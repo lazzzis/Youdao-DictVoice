8421e74f8987e42e096a971f0444f3934a6f798d
lazzzis <shenlijin@outlook.com>
2016-12-22 20:50:27 +0800

    implement a parser for arguments

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # DictVoice
 
-从 [youdao.com](http://youdao.com) 获取单词或词组的音频，可以用于简单的听写
+从 [youdao.com](http://youdao.com) 获取单词或词组的音频并自动播放，可以用于简单的听写
 
 # 安装
 
@@ -28,7 +28,7 @@ brew install ffmpeg
 
 # 使用方法
 
-在当前目录建立一个文件，名为 words.md。文件中每个单词一行。
+在当前目录建立一个文件，名为 words.txt. 文件中每个单词一行。
 
 执行文件
 
@@ -36,12 +36,47 @@ brew install ffmpeg
 python3 dictVoice.py
 ```
 
-# 参数更改
+# 可選參數
 
-打开文件，可在文件开头（前10行）中看到两个参数：
+```bash
+usage: dictVoice.py [-h] [-v INTERVAL_TIME] [-f FILE] [-o OUTPUT] [-rd] [-s]
+                    [-rs] [-no] [-ro]
 
-- interval_time: 两个单词声音之间的间隙，默认为 1，单位为秒，即一个单词读完后，停顿一秒后在读下一个单词
-- words_file: 读取单词的文件的名字，默认为 "words.md"
+optional arguments:
+  -h, --help            show this help message and exit
+  -v INTERVAL_TIME, --interval-time INTERVAL_TIME
+                        the interval time (seconds) between two words
+                        (default: 1)
+  -f FILE, --file FILE  specify the origin of words (default: words.txt)
+  -o OUTPUT, --output OUTPUT
+                        specify a file storing words with actual order
+                        (default ans.txt)
+  -rd, --random         play words according to the random order
+  -s, --sort            sort all the words in alphabetical order
+  -rs, --reverse-sort   sort reversely all the words in alphabetical order
+  -no, --normal-order   play words according to order of the appearance in
+                        file (default)
+  -ro, --reverse-order  play words according to reverse order of the
+                        appearance in file
+```
+
+```bash
+usage: dictVoice.py [-h] [-v INTERVAL_TIME] [-f FILE] [-o OUTPUT] [-rd] [-s]
+                    [-rs] [-no] [-ro]
+
+optional arguments:
+  -h, --help            显示帮助
+  -v INTERVAL_TIME, --interval-time INTERVAL_TIME
+                        播放两个单词之间的时长，默认为 1 秒
+  -f FILE, --file FILE  指定单词的输入文本源，默认为"words.txt"
+  -o OUTPUT, --output OUTPUT
+                        指定文件用于存储单词（以实际播放的顺序为准），默认为"ans.txt"
+  -rd, --random         随机顺序播放单词
+  -s, --sort            以字典序排列所有单词
+  -rs, --reverse-sort   以字典序逆序排列所有单词
+  -no, --normal-order   根据文件中实际顺序播放单词（默认）
+  -ro, --reverse-order  根据文件中实际顺序的逆序播放单词
+```
 
 # 删除下载的文件
 
@@ -51,3 +86,6 @@ python3 dictVoice.py
 rm *.wav
 ```
 
+## TODO
+
+- [ ] add test
--- dictVoice.py
+++ dictVoice.py
@@ -1,12 +1,12 @@
-from pydub import AudioSegment
+import argparse
 import os
-import requests
-import simpleaudio as sa
+import random
 import time
 from collections import Iterable
 
-words_file = 'words.md'
-interval_time = 1  # the internal time between two words
+import requests
+import simpleaudio as sa
+from pydub import AudioSegment
 
 
 def check_cache(f):
@@ -43,13 +43,91 @@ def play_audio(audio, wait=True, sleep=0):
     play_obj = wave_obj.play()
     if wait:
         play_obj.wait_done()
-    time.sleep(sleep)
 
 
-with open(words_file) as f:
-    lst = f.readlines()
-    lst = (item.strip() for item in lst)
-    lst = [item for item in lst if item != '']
-download_audio(lst)
-for item in (item + '.wav' for item in lst):
-    play_audio(item, sleep=interval_time)
+def make_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--interval-time",
+        help="the interval time (seconds) between two words (default: 1)",
+        type=int,
+        default=1)
+
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="specify the origin of words (default: words.txt)",
+        type=str,
+        default="words.txt")
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="specify a file storing words with actual order (default ans.txt)",
+        type=str,
+        default="ans.txt")
+
+    parser.add_argument(
+        "-rd",
+        "--random",
+        help="play words according to the random order",
+        action="store_true",
+        default=False)
+
+    parser.add_argument(
+        "-s",
+        "--sort",
+        help="sort all the words in alphabetical order",
+        action="store_true",
+        default=False)
+
+    parser.add_argument(
+        "-rs",
+        "--reverse-sort",
+        help="sort reversely all the words in alphabetical order",
+        action="store_true",
+        default=False)
+
+    parser.add_argument(
+        "-no",
+        "--normal-order",
+        help="play words according to order of the appearance in file (default)",
+        action="store_true",
+        default=True)
+
+    parser.add_argument(
+        "-ro",
+        "--reverse-order",
+        help="play words according to reverse order of the appearance in file",
+        action="store_true",
+        default=False)
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = make_parser()
+    args = parser.parse_args()
+
+    with open(args.file) as f:
+        lst = f.readlines()
+        lst = (item.strip() for item in lst)
+        lst = (item for item in lst if item != '')
+
+    if args.random:
+        lst = list(lst)
+        random.shuffle(lst)
+    elif args.reverse_order:
+        lst = list(lst)[::-1]
+    elif args.sort or args.reverse_sort:
+        lst = sorted(lst, reverse=args.reverse_sort)
+
+    download_audio(lst)
+    for item in (item + '.wav' for item in lst):
+        play_audio(item)
+        time.sleep(args.interval_time)
+
+    if args.output:
+        with open(args.output, 'w+') as f:
+            f.write("\n".join(lst))
